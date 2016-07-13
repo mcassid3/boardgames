1163b3cf193424f12c878218c2f8afa4e5ab6b2e
Mark Cassidy <mcassid3@gmail.com>
2016-07-13 13:43:36 -0500

    travis first test

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe GamesController, type: :controller do
-
+  it "GET#index functions" do
+    get :index
+    expect(response).to have_http_status(200)
+  end
 end
