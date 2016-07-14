140652fa6bd705a6706d737c151249f8dd5d5f98
Kyriakos Kalpakidis <kjkalpa@gmail.com>
2016-07-14 10:30:25 -0500

    Add comment form and partials

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,9 @@ class CommentsController < ApplicationController
 
   def create
     @game = Game.find(params[:game_id])
-    @comment = @game.comments.create(params[:comment].permit(:name, :body))
+    @comment = @game.comments.build(params[:comment].permit(:name, :body))
+    @comment.user_id = current_user.id
+    @comment.save
 
     redirect_to game_path(@game)
   end
--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,8 +4,6 @@ class GamesController < ApplicationController
   end
 
   def show
-    p session[:id]
     @game = Game.find(params[:id])
-    # @game.comments.user_id = session[:id]
   end
 end
